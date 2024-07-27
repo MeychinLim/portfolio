5dc742a2dae11ccf568e52c9a4f84eb43dee90c4
Meychin Lim <limmeychin2001@gmail.com>
2024-07-27 15:50:28 +0700

    style: set duration to 500ms for hover social media

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -5,16 +5,16 @@ function SocialMedia() {
         <div className='md:fixed top-96 right-10 z-50'>
             <div className='flex flex-row md:flex-col gap-4'>
                 <a href='https://t.me/meychinlim' target='_blank'>
-                    <Telegram className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-300 block' />
+                    <Telegram className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-500 block' />
                 </a>
                 <a
                     href='https://www.linkedin.com/in/meychin-lim-b9a81125b/'
                     target='_blank'
                 >
-                    <Linkedin className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-300 block' />
+                    <Linkedin className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-500 block' />
                 </a>
                 <a href='https://github.com/meychinlim' target='_blank'>
-                    <GithubCircle className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-300 block' />
+                    <GithubCircle className='w-10 h-10 hover:text-accent hover:scale-125 transition-transform duration-500 block' />
                 </a>
             </div>
         </div>
