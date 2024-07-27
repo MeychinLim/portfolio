c2a05b98bc47caf6f6fc64c4007ab2842bff40d5
Meychin Lim <limmeychin2001@gmail.com>
2024-07-27 15:48:28 +0700

    fix: got warning using dependancies variable directly

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,15 @@
-import { Instagram, X } from 'iconoir-react';
-import { dependencies } from '@/../package.json';
+import next from '@/../package.json';
 import SocialMedia from './SocialMedia';
 
 function Footer() {
     return (
-        <div className='bottom-0 w-full font-mono'>
+        <div className='w-full font-mono'>
             <footer className='footer footer-center p-10 bg-gray-400/10 rounded'>
                 <div className='block md:hidden'>
                     <SocialMedia />
                 </div>
                 <aside>
-                    <p>Built with NextJs {dependencies?.next}</p>
+                    <p>Built with NextJs {next?.dependencies?.next}</p>
                 </aside>
             </footer>
         </div>
